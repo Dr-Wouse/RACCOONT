5efbc5725020b439c1ce640aa9de8e8161b502b6
Dr Wouse <e.gon66@outlook.fr>
2022-02-03 20:21:27 +0100

    Project finish ! Follow and unfollow doesnt work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# RACCOONT
+WireFrame : https://www.figma.com/file/yzDzf1o7DAXSL6Dw43tgwb/Untitled?node-id=0%3A1
+
+npm start puis cd client => npm start
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const cors = require("cors");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/database");
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+
+// A chaque requête le user va être vérifié
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Ecoute le port ${process.env.PORT}`);
+});
